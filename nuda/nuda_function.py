6de0958b1c05cbd48c9115e16e146b24bc25636c
Weten-Ogen <alphaoware@gmail.com>
2025-06-03 12:14:22 +0000

    first commit

---
+++ nuda/nuda_function.py
@@ -0,0 +1,37 @@
+# authenticate users
+def authenticate():
+    """
+        *** Authenticate 
+        authenticate users using bcrypt library to check if 
+        the user has enter the right password
+    """
+     pass
+
+
+# create a session token 
+def generate_token():
+    """
+        ***Generate_token
+        return the special token key that will be used to pass the user 
+        as either authenticated or needs to be reauthenticated
+    """
+    pass
+
+#reset token
+def reset_token():
+    """
+        ***Reset_token
+        reset the credentials with new token when it expires.
+    """
+    pass
+
+# reset password
+def forgotten_password():
+    """
+        ***
+    """
+    pass
+
+# delete credentials
+def delete_credentials():
+    pass
